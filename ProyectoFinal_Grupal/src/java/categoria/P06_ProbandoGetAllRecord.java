2f26e52693464e1345f1de175971efed11455659
Matias <mkluna@institutosanmartin.edu.ar>
2019-10-30 01:59:09 -0300

    Pruebas de GetAllRecord y GetJoinAll

---
+++ ProyectoFinal_Grupal/src/java/categoria/P06_ProbandoGetAllRecord.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package categoria;
+
+import daogenerico.ErroresGenerales;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Matias
+ */
+public class P06_ProbandoGetAllRecord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+     List<categoria> ListaDeVuelta = new ArrayList<categoria>();
+     categoriaDAO MyDAO = new categoriaDAO();
+        try {
+            ListaDeVuelta = MyDAO.getAll("select * from categoria");
+            if (ListaDeVuelta != null) {
+                for (int i = 0; i < ListaDeVuelta.size(); i++) {
+                    System.out.println(ListaDeVuelta.get(i).toString());
+                }
+            } else {
+                System.out.println("NO LO ENCONTRAMOS");
+            }
+        } catch (ErroresGenerales ex) {
+            System.out.println(ex.getMensajeDeErrorSalida());
+            System.out.println("HUBO UN ERROR !!! " + ex.getMensajeDeErrorGeneral());
+    }
+    
+}
+}

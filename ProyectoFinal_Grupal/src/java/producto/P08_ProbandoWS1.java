e5bb2de102faade20c61f9325b14fb0287cc4943
LucasDanielMontivero <55299285+LucasDanielMontivero@users.noreply.github.com>
2019-11-01 17:27:31 -0300

    conversor de obj java a json y prueba realizada en productoWS tipo proceso 1

---
+++ ProyectoFinal_Grupal/src/java/producto/P08_ProbandoWS1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package producto;
+
+import com.google.gson.Gson;
+
+/**
+ *
+ * @author Monti
+ */
+public class P08_ProbandoWS1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*LO QUE HACE EESTO ES CONVERTIR EN OBJ PRODUCTO EN UN OBJETO JSON PARA PASARLO COMO
+        PARAMETRO EN EL WS ?TipoProceso=1&ParametroJSON= ...*/
+        
+        producto P = new producto(Long.valueOf(10), "Fernisito coooordobes", "00000001", "250", "01/11/2019",Long.valueOf(1));
+        Gson G = new Gson();
+        
+        String productoEnFormatoJSON = G.toJson(P);
+        System.out.println(productoEnFormatoJSON);
+        
+        
+        
+        
+    }
+    
+}
